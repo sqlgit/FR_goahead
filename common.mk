d3786f3ca1d29284c42955a696d89ba4096a23d3
sql <shiqiliang@frtech.fr>
2019-12-08 22:06:27 -0800

    Before unifying the code into the robot directory, and modifying the associated compilation path

--- common.mk
+++ common.mk
@@ -7,13 +7,18 @@ QCONFIG=qconfig.mk
 endif
 include $(QCONFIG)
 
+#===== USEFILE - the file containing the usage message for the application. 
 USEFILE=
 
+#===== EXTRA_INCVPATH - a space-separated list of directories to search for include files.
 EXTRA_INCVPATH+=$(PROJECT_ROOT)/src  \
-	$(PROJECT_ROOT)/src/osdep
+	$(PROJECT_ROOT)/src/osdep $(PROJECT_ROOT)/src/robot
 
-EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src
+#===== EXTRA_SRCVPATH - a space-separated list of directories to search for source files.
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src  \
+	$(PROJECT_ROOT)/src/robot
 
+#===== LIBS - a space-separated list of library items to be included in the link.
 LIBS+=m socks socket
 
 include $(MKFILES_ROOT)/qmacros.mk
