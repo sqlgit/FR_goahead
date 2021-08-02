01cbb31f4f1babd62d8bbc437ece489b85e2540e
sql <shiqiliang@frtech.fr>
2021-08-02 19:54:17 +0800

    增加机器人干涉区功能相关指令

--- src/robot/action_set.c
+++ src/robot/action_set.c
@@ -2119,7 +2119,7 @@ void set(Webs *wp)
 		}
 	// cmd_auth "2"
 	}
-	if (cmd == 320 || cmd == 201 || cmd == 303 || cmd == 101 || cmd == 102 || cmd == 103 || cmd == 104 || cmd == 107 || cmd == 1001 || cmd == 232 || cmd == 233 || cmd == 208 || cmd == 216 || cmd == 203 || cmd == 204 || cmd == 209 || cmd == 210 || cmd == 211 || cmd == 234 || cmd == 316 || cmd == 306 || cmd == 307 || cmd == 206 || cmd == 305 || cmd == 321 || cmd == 323 || cmd == 324 || cmd == 325 || cmd == 222 || cmd == 223 || cmd == 224 || cmd == 225 || cmd == 105 || cmd == 106 || cmd == 315 || cmd == 317 || cmd == 318 || cmd == 226 || cmd == 229 || cmd == 227 || cmd == 330 || cmd == 235 || cmd == 236 || cmd == 237 || cmd == 238 || cmd == 239 || cmd == 240 || cmd == 247 || cmd == 248 || cmd == 249 || cmd == 250 || cmd == 251 || cmd == 252 || cmd == 253 || cmd == 254 || cmd == 255 || cmd == 256 || cmd == 257 || cmd == 258 || cmd == 259 || cmd == 260 || cmd == 265 || cmd == 266 || cmd == 267 || cmd == 268 || cmd == 269 ||  cmd == 270 || cmd == 278 || cmd == 279 || cmd == 283 || cmd == 287 || cmd == 292 || cmd == 293 || cmd == 294 || cmd == 295 || cmd == 296 || cmd == 297 || cmd == 298 || cmd == 333 || cmd == 334 || cmd == 335 || cmd == 336 || cmd == 337 || cmd == 338 || cmd == 339 || cmd == 340 || cmd == 341 || cmd == 343 || cmd == 353 || cmd == 354 || cmd == 355 || cmd == 356|| cmd == 357 || cmd == 358 || cmd == 359 || cmd == 360 || cmd == 361 || cmd == 362 || cmd == 367 || cmd == 368 || cmd == 369 || cmd == 370 || cmd == 371 || cmd == 372 || cmd == 375 || cmd == 376 || cmd == 380 || cmd == 381 || cmd == 382 || cmd == 384 || cmd == 386 || cmd == 387 || cmd == 388 || cmd == 389 || cmd == 390 || cmd == 391 || cmd == 393 || cmd == 401 || cmd == 402 || cmd == 403 || cmd == 404 || cmd == 405 || cmd == 406 || cmd == 407 || cmd == 408 || cmd == 409 || cmd == 410 || cmd == 411 || cmd == 412 || cmd == 413 || cmd == 414 || cmd == 415 || cmd == 422 || cmd == 423 || cmd == 424 || cmd == 426 || cmd == 427) {
+	if (cmd == 320 || cmd == 201 || cmd == 303 || cmd == 101 || cmd == 102 || cmd == 103 || cmd == 104 || cmd == 107 || cmd == 1001 || cmd == 232 || cmd == 233 || cmd == 208 || cmd == 216 || cmd == 203 || cmd == 204 || cmd == 209 || cmd == 210 || cmd == 211 || cmd == 234 || cmd == 316 || cmd == 306 || cmd == 307 || cmd == 206 || cmd == 305 || cmd == 321 || cmd == 323 || cmd == 324 || cmd == 325 || cmd == 222 || cmd == 223 || cmd == 224 || cmd == 225 || cmd == 105 || cmd == 106 || cmd == 315 || cmd == 317 || cmd == 318 || cmd == 226 || cmd == 229 || cmd == 227 || cmd == 330 || cmd == 235 || cmd == 236 || cmd == 237 || cmd == 238 || cmd == 239 || cmd == 240 || cmd == 247 || cmd == 248 || cmd == 249 || cmd == 250 || cmd == 251 || cmd == 252 || cmd == 253 || cmd == 254 || cmd == 255 || cmd == 256 || cmd == 257 || cmd == 258 || cmd == 259 || cmd == 260 || cmd == 265 || cmd == 266 || cmd == 267 || cmd == 268 || cmd == 269 ||  cmd == 270 || cmd == 278 || cmd == 279 || cmd == 283 || cmd == 287 || cmd == 292 || cmd == 293 || cmd == 294 || cmd == 295 || cmd == 296 || cmd == 297 || cmd == 298 || cmd == 333 || cmd == 334 || cmd == 335 || cmd == 336 || cmd == 337 || cmd == 338 || cmd == 339 || cmd == 340 || cmd == 341 || cmd == 343 || cmd == 353 || cmd == 354 || cmd == 355 || cmd == 356|| cmd == 357 || cmd == 358 || cmd == 359 || cmd == 360 || cmd == 361 || cmd == 362 || cmd == 367 || cmd == 368 || cmd == 369 || cmd == 370 || cmd == 371 || cmd == 372 || cmd == 375 || cmd == 376 || cmd == 380 || cmd == 381 || cmd == 382 || cmd == 384 || cmd == 386 || cmd == 387 || cmd == 388 || cmd == 389 || cmd == 390 || cmd == 391 || cmd == 393 || cmd == 401 || cmd == 402 || cmd == 403 || cmd == 404 || cmd == 405 || cmd == 406 || cmd == 407 || cmd == 408 || cmd == 409 || cmd == 410 || cmd == 411 || cmd == 412 || cmd == 413 || cmd == 414 || cmd == 415 || cmd == 422 || cmd == 423 || cmd == 424 || cmd == 426 || cmd == 427 || cmd == 430 || cmd == 431 || cmd == 432 || cmd == 433 || cmd == 434 || cmd == 435 || cmd == 436) {
 		if (!authority_management("2")) {
 			perror("authority_management");
 			goto auth_end;
@@ -3336,6 +3336,55 @@ void set(Webs *wp)
 		strcpy(log_content, "获取机器人作业原点");
 		strcpy(en_log_content, "Obtain the origin of robot operation");
 		strcpy(jap_log_content, "ロボット作業の原点を取得する");
+		ret = copy_content(data_json, content);
+		break;
+	case 430:
+		port = cmdport;
+		strcpy(log_content, "轴干涉区开关设置");
+		strcpy(en_log_content, "Axis interference zone switch setting");
+		strcpy(jap_log_content, "軸干渉型スイッチ設定");
+		ret = copy_content(data_json, content);
+		break;
+	case 431:
+		port = cmdport;
+		strcpy(log_content, "轴干涉区参数设置");
+		strcpy(en_log_content, "Axis interference zone parameter setting");
+		strcpy(jap_log_content, "軸干渉領域のパラメータ設定");
+		ret = copy_content(data_json, content);
+		break;
+	case 432:
+		port = cmdport;
+		strcpy(log_content, "立方体干涉区开关设置");
+		strcpy(en_log_content, "Cube interference zone switch setting");
+		strcpy(jap_log_content, "立方体干渉スイッチ設定");
+		ret = copy_content(data_json, content);
+		break;
+	case 433:
+		port = cmdport;
+		strcpy(log_content, "立方体干涉区参数设置");
+		strcpy(en_log_content, "Cube interference zone parameter setting");
+		strcpy(jap_log_content, "立方体干渉領域のパラメータ設定");
+		ret = copy_content(data_json, content);
+		break;
+	case 434:
+		port = cmdport;
+		strcpy(log_content, "立方体干涉区范围设置 - 两点法");
+		strcpy(en_log_content, "Cube interference zone range setting - two point method");
+		strcpy(jap_log_content, "立方体干渉領域の範囲を設定する - 2点法");
+		ret = copy_content(data_json, content);
+		break;
+	case 435:
+		port = cmdport;
+		strcpy(log_content, "立方体干涉区范围设置 - 一点+边长");
+		strcpy(en_log_content, "Cube interference zone range setting - one point + side length");
+		strcpy(jap_log_content, "立方体の干渉領域の範囲を設定 - 一点+辺の長さ");
+		ret = copy_content(data_json, content);
+		break;
+	case 436:
+		port = cmdport;
+		strcpy(log_content, "进入干涉区是否停止设置");
+		strcpy(en_log_content, "Whether to stop setting when entering the interference zone");
+		strcpy(jap_log_content, "干渉領域に入るかどうかの設定を停止します");
 		ret = copy_content(data_json, content);
 		break;
 	case 511:
